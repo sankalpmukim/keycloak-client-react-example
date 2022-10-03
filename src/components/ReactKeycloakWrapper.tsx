0ddda57e81acc73814d6a4774b74b7f417336972
Sankalp Mukim <sankalpmukim@gmail.com>
2022-10-03 18:53:16 +0530

    Login, Logout functioning

---
+++ src/components/ReactKeycloakWrapper.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, useEffect, useState } from "react";
+import keycloak from "../keycloak";
+
+async function initKeycloak() {
+  return await keycloak
+    .init({})
+    .then(function (authenticated) {
+      return authenticated;
+    })
+    .catch(function () {
+      console.log("failed to initialize");
+    });
+}
+
+const ReactKeycloakWrapper = ({ children }: { children: ReactNode }) => {
+  const [authenticated, setAuthenticated] = useState(false);
+  useEffect(() => {
+    const init = async () => {
+      const authenticated = await initKeycloak();
+      if (typeof authenticated !== "undefined") {
+        setAuthenticated(authenticated);
+      }
+    };
+    init();
+  }, []);
+  return (
+    <div>
+      {authenticated ? (
+        <div>
+          <div>{`Logout`}</div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              keycloak.logout();
+            }}
+          >{`Logout`}</button>
+        </div>
+      ) : (
+        <>
+          <div>{`Authenticated ${authenticated}`}</div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              keycloak.login();
+            }}
+          >{`Login`}</button>
+          {children}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ReactKeycloakWrapper;
--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -1,37 +1,13 @@
 import { useState, useEffect } from "react";
 import Keycloak from "keycloak-js";
+import ReactKeycloakWrapper from "./ReactKeycloakWrapper";
 
 export default function Resources() {
-  const [keycloak, setKeycloak] = useState<Keycloak | null>(null);
-  const [authenticated, setAuthenticated] = useState(false);
-
-  useEffect(() => {
-    const keycloak = Keycloak("/keycloak.json");
-    keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
-      setKeycloak(keycloak);
-      setAuthenticated(authenticated);
-    });
-  }, []);
-
-  if (keycloak) {
-    if (authenticated)
-      return (
-        <>
-          {/* JSX returns an image and text as protected **resources** */}
-          <div className="my-12 grid place-items-center">
-            <p> You are logged in.</p>
-            <div>
-              <img src="https://random.imagecdn.app/500/250" />
-            </div>
-          </div>
-        </>
-      );
-    else return <div className="my-12">Unable to initiate auth!</div>;
-  }
-
   return (
-    <>
-      <div className="my-12">Keycloak initializing in a moment...</div>
-    </>
+    <div>
+      <ReactKeycloakWrapper>
+        <div className="my-12">Keycloak initializing in a moment...</div>
+      </ReactKeycloakWrapper>
+    </div>
   );
 }
